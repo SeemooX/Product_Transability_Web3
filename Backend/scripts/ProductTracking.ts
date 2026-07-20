18e14d7c5822ad885316b3392e2277238dbe4910
SeemooX <bjjsimox@gmail.com>
2026-07-20 16:14:47 +0100

    Adding product creation to deployment script for testing

--- Backend/scripts/ProductTracking.ts
+++ Backend/scripts/ProductTracking.ts
@@ -1,14 +1,36 @@
 import { network } from "hardhat";
+/* import { id } from "ethers"; */
 
 const { ethers, networkName } = await network.create();
 
 console.log(`Deploying ProductTracking to ${networkName}...`);
 
-const counter = await ethers.deployContract("ProductTracking");
+const contract = await ethers.deployContract("ProductTracking");
 
-console.log("Waiting for the deployment tx to confirm");
-await counter.waitForDeployment();
+console.log("Waiting for deployment...");
+await contract.waitForDeployment();
 
-console.log("ProductTracking address:", await counter.getAddress());
+const address = await contract.getAddress();
 
-console.log("Deployment successful!");
+console.log("ProductTracking deployed at:", address);
+
+// Convert the UUID to bytes32
+/* const productId = id("4a26a2e0-e8ee-4e86-b9b3-473f66640cdc");
+
+const metadataHash =
+    "0x9e659fd7f02ddc9454e057d0f9969f7436d6662cd03d742bce78551129dc48aa";
+
+const eventHash =
+    "0x0c57aa3029f452812c942853c0ad0254229a8bd9b081bc1ded8cb254ca34a67c";
+
+console.log("Creating product...");
+
+const tx = await contract.createProduct(
+    productId,
+    metadataHash,
+    eventHash
+);
+
+await tx.wait();
+
+console.log("Product created successfully!"); */
