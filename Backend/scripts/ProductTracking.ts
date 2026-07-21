bb13e718aed6d718c52eed1851b708b85439972e
SeemooX <bjjsimox@gmail.com>
2026-07-21 23:14:42 +0100

    Building the deployyment script and execute functions for testing purpose

--- Backend/scripts/ProductTracking.ts
+++ Backend/scripts/ProductTracking.ts
@@ -1,5 +1,5 @@
 import { network } from "hardhat";
-/* import { id } from "ethers"; */
+import { id } from "ethers";
 
 const { ethers, networkName } = await network.create();
 
@@ -15,7 +15,7 @@ const address = await contract.getAddress();
 console.log("ProductTracking deployed at:", address);
 
 // Convert the UUID to bytes32
-/* const productId = id("4a26a2e0-e8ee-4e86-b9b3-473f66640cdc");
+const productId = id("45dcd27b-2c2c-4325-955f-a89a537dbce3");
 
 const metadataHash =
     "0x9e659fd7f02ddc9454e057d0f9969f7436d6662cd03d742bce78551129dc48aa";
@@ -33,4 +33,18 @@ const tx = await contract.createProduct(
 
 await tx.wait();
 
-console.log("Product created successfully!"); */
+console.log("Product created successfully!");
+
+console.log("updating product...");
+
+const newEventHash = "0x4a1a294441937a04899bd6a7ed83bc1bb3ba4dfc8dc98832aacfd067a939c5b1";
+
+const tx1 = await contract.addTraceabilityEvent(
+    productId,
+    2,
+    newEventHash
+);
+
+await tx1.wait();
+
+console.log("Product updated successfully!");
