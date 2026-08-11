b18085849eeeafd08cf24339bce4a9edecee71ff
SeemooX <bjjsimox@gmail.com>
2026-08-11 16:30:53 +0100

    Installing the needed dependencies of capacitor

---
+++ Front__End/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.TraceProduct.com',
+  appName: 'TraceProduct',
+  webDir: 'dist'
+};
+
+export default config;
