d5e382bc88833bd4052158712c9805ea0f039ff2
SeemooX <bjjsimox@gmail.com>
2026-08-24 13:41:34 +0100

    small refactoring

--- Backend2/middlewares/credentials.js
+++ Backend2/middlewares/credentials.js
@@ -1,7 +1,6 @@
 const {allowedOrigins} = require('../config/allowedOrigins')
 
 const credentials = (req, res, next) => {
-    console.log('Origin:', req.headers.origin);
     const origin = req.headers.origin;
     if(allowedOrigins.includes(origin)) {
         res.setHeader('Access-Control-Allow-Credentials', 'true');
