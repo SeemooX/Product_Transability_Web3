e987d5cae24060dcd1c5ec4f1e88aaa497866b83
SeemooX <bjjsimox@gmail.com>
2026-08-28 13:10:06 +0100

    Testing

--- Backend2/config/allowedOrigins.js
+++ Backend2/config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:3000',
     'http://127.0.0.1:5500',,
-    'http://localhost'
+    /* 'http://localhost', */
+    'https://localhost',
 ];
 
 module.exports = { allowedOrigins }
